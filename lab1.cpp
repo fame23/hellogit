aec2261f8146786373e3f12ae27886e922c77851
fame23 <fame23@users.noreply.github.com>
2019-03-04 16:59:54 -0400

    lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -1,26 +1,26 @@
 #include <vector>
 #include <iostream>
 #include <string>
-using namespace std
-main()
+using namespace std;
+int main()
 {
 	string s0 = "nothing";
 	string name = "Kyle";
 	string fname = name + "Holder";
-	vector <int> vect = {23,24,56,48,92};
+	vector <int> vect;
 	vector <char> vc;
 	
 	vc.push_back('p');
 	for (int i=0; i<5;i++)
 	{
-		vc.pop_back();
+		vect.push_back(34);
 	}
 	
 	
 	cout << "s0 =" << s0 << endl;
 	cout << "name =" << name << endl;
 	cout << "The 3rd character of name is " << name[3] << endl;
-	cout << "name has " << name.size() << "characters"" << endl;
+	cout << "name has " << name.size() << "characters" << endl;
 	
 	if (name == "Kyle")
 	{
